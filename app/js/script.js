f2681733f0bc39d358bdde63370a75d8b2529e8a
lonelybuddy <henryzheng92@gmail.com>
2021-11-23 20:02:12 -0600

    finished header

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,27 @@
+const hamburger = document.querySelector('#hamburger');
 const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+const fadeElms = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
-header.addEventListener('click', toggleMenu);
+hamburger.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
-  header.classList.toggle('open');
+  if (header.classList.contains('open')) {
+    // Close Hamburger Menu
+    body.classList.remove('noscroll');
+    header.classList.remove('open');
+    fadeElms.forEach((element) => {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+  } else {
+    // Open Hamburger Menu'
+    body.classList.add('noscroll');
+    header.classList.add('open');
+    fadeElms.forEach((element) => {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    });
+  }
 }
